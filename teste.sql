ddb07ef093418a46add9a27a09867720498b032a
ryudi <eduardoryudi@gmail.com>
2025-08-22 12:11:47 -0300

    Atualizando teste.sql

--- teste.sql
+++ teste.sql
@@ -15,3 +15,6 @@ CREATE TABLE IF NOT EXISTS stations (
     status VARCHAR(100) NOT NULL,            
     uf CHAR(2) NOT NULL                    
 );
+
+INSERT INTO users (email,password_hash) 
+VALUES ('teste@teste.com', 'scrypt:32768:8:1$Do2PnNPVTVh8g1CJ$e0e1b8f750533fab6837df8ebd9cc23dd7c37161ef21a617c273eb31a378d05243b21539f03b11651dfea345165b30d2fe0b6954f0f7f86868ff4d11332fd397')
